62e332206549400ae698b317eeaa65c8d4cb492d
Rayssa <manoinoc@gmail.com>
2024-09-22 03:10:21 -0300

    alteração 2

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 function beberLeve() {
-    // Altera a imagem para feliz
     document.getElementById("avatar").src = "uffa.jpeg";
-    // Exibe a mensagem positiva
     document.getElementById("mensagem").innerHTML = "Fico feliz! isso ai.. S2 ";
 }
 
 function beberMuito() {
-    // Altera a imagem para bravo
     document.getElementById("avatar").src = "brava.jpeg";
-    // Exibe a mensagem de aviso
     document.getElementById("mensagem").innerHTML = "Aiiii, você não ajuda Jão! Você trabalha amanhã! Se cuida.";
 }
